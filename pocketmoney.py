5d7d321952f438dd538e97b038330389750d6b8b
harkirat1984 <71445577+harkirat1984@users.noreply.github.com>
2021-06-29 18:09:33 +0530

    Add files via upload

---
+++ pocketmoney.py
@@ -0,0 +1,10 @@
+pocketmoney=int(input("enter your pocket money: "))
+if(pocketmoney>500):
+     print("give me your money")
+
+elif(pocketmoney>100):
+    print("i still want the moneyyy")
+
+else:
+    print("0 money ? still give me some money")    
+
